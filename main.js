0d2df4d1aac80c9c46a860522f33a472146597f4
Razeb Enyi <enyirazeb@gmail.com>
2020-07-14 09:23:42 +0100

    Update main.js

--- main.js
+++ main.js
@@ -19,33 +19,15 @@ $(function () {
         }, 1250, "easeInOutExpo")
     })
 })
-AOS.init();
 
-hljs.initHighlightingOnLoad();
-
-$('.hero__scroll').on('click', function (e) {
-    $('html, body').animate({
-        scrollTop: $(window).height()
-    }, 1200);
-});
-
-$(document).ready(function () {
-    // Check if element is scrolled into view
-    function isScrolledIntoView(elem) {
-        var docViewTop = $(window).scrollTop();
-        var docViewBottom = docViewTop + $(window).height();
-
-        var elemTop = $(elem).offset().top;
-        var elemBottom = elemTop + $(elem).height();
-
-        return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
-    }
-    // If element is scrolled into view, fade it in
-    $(window).scroll(function () {
-        $('.scroll-animations .animated').each(function () {
-            if (isScrolledIntoView(this) === true) {
-                $(this).addClass('fadeInLeft');
-            }
-        });
+$(function () {
+    $("#solfas-testimonial").owlCarousel({
+        items: 3,
+        autoPlay: true,
+        smartSpeed: 400,
+        look: true,
+        autoPlayHoverPause: true,
     });
 });
+
+
